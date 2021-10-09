f5e35842b21e231d2f80fdfd03e7a2e6b82cb7f6
trozans-trap <chawlavikalp2000@gmail.com>
2021-10-09 23:27:50 +0530

    basic validation

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 app.set('view engine','ejs');
 
-app.use('/tipCalculator',require('./routes/api'))
 
+app.use('/tipCalculator',require('./routes/api'))
+app.use(express.static(__dirname + '/public'));
 const port = process.env.PORT||3000;
 
 app.listen(port,()=>{
