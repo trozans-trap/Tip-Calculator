1d495020c744076b4b25df3f472bbcb10bb10e90
trozans-trap <chawlavikalp2000@gmail.com>
2021-10-10 22:33:43 +0530

    edcase validatiion

--- Public/script.js
+++ Public/script.js
@@ -1,20 +1,21 @@
 // console.log("HI");
-$(document).ready(function(){
-    $('.minus').click(function () {
-        var $input = $(this).parent().find('input');
-        var count = parseInt($input.val()) - 1;
-        count = count < 1 ? 1 : count;
-        $input.val(count);
-        $input.change();
+let decr = (flag,id)=>{
+    let val = document.getElementById(id).value;
+    if(val<=0){
+        alert("Can't be in Negative .");
         return false;
-    });
-    $('.plus').click(function () {
-        var $input = $(this).parent().find('input');
-        $input.val(parseInt($input.val()) + 1);
-        $input.change();
+    }
+    document.getElementById(id).value=parseInt(val)-1;
+}
+
+let incr = (flag,id)=>{
+    let val = document.getElementById(id).value;
+    if(val>=100 && flag==0){
+        alert("Can't be more than ",val);
         return false;
-    });
-});
+    }
+    document.getElementById(id).value=parseInt(val)+1;
+};
 
 let billVal = 0;
 let tipPerc = 0;
@@ -22,22 +23,62 @@ let peopleNum = 0;
 console.log(billVal);
 console.log(tipPerc);
 console.log(peopleNum);
+let si = [
+    {v: 1E3, s: "K"},
+    {v: 1E6, s: "M"},
+    {v: 1E9, s: "B"},
+    {v: 1E12, s: "T"},
+    {v: 1E15, s: "P"},
+    {v: 1E18, s: "E"}
+];
+
+let convertBig = (num)=>{
+    num = num.toString().replace(/[^0-9.]/g, '');
+    let i;
+    for (i = si.length - 1; i > 0; i--) {
+        if (num >= si[i].v) {
+            break;
+        }
+    }
+    return (num / si[i].v).toFixed(2).replace(/\.0+$|(\.[0-9]*[1-9])0+$/, "$1") + si[i].s;
+}
 
 let billValChange = ()=>{
     billVal = document.getElementById("billVal").value;
-    document.getElementById("tipVal").innerHTML = billVal*tipPerc/peopleNum;
-    document.getElementById("totalPerPerson").innerHTML = billVal/peopleNum +billVal*tipPerc/peopleNum;
+    let tipVal = 0,totalPerPerson=0;
+    if(peopleNum>0){
+        tipVal = Math.round(((billVal*tipPerc)/100)*100)/100;
+        totalPerPerson = Math.round(((billVal/peopleNum) +(billVal*tipPerc)/100)*100)/100;
+    }
+    if(tipVal>1000)tipVal=convertBig(tipVal);
+    if(totalPerPerson>1000)totalPerPerson=convertBig(totalPerPerson);
+    document.getElementById("tipVal").innerHTML = tipVal;
+    document.getElementById("totalPerPerson").innerHTML = totalPerPerson;
 }
 
 let tipChange = ()=>{
     tipPerc = document.getElementById("tipPerc").value;
-    document.getElementById("tipVal").innerHTML = billVal*tipPerc/100;
-    document.getElementById("totalPerPerson").innerHTML = Math.round(((billVal/peopleNum) +(billVal*tipPerc)/100)*100)/100;
+    let tipVal = 0,totalPerPerson=0;
+    if(peopleNum>0){
+        tipVal = Math.round(((billVal*tipPerc)/100)*100)/100;
+        totalPerPerson = Math.round(((billVal/peopleNum) +(billVal*tipPerc)/100)*100)/100;
+    }
+    if(tipVal>1000)tipVal=convertBig(tipVal);
+    if(totalPerPerson>1000)totalPerPerson=convertBig(totalPerPerson);
+    document.getElementById("tipVal").innerHTML = tipVal;
+    document.getElementById("totalPerPerson").innerHTML = totalPerPerson;
 }
 
 let peopleNumChange = ()=>{
     peopleNum = document.getElementById("peopleNum").value;
-    document.getElementById("tipVal").innerHTML = billVal*tipPerc/100;
-    document.getElementById("totalPerPerson").innerHTML = Math.round(((billVal/peopleNum) +(billVal*tipPerc)/100)*100)/100;
+    let tipVal = 0,totalPerPerson=0;
+    if(peopleNum>0){
+        tipVal = Math.round(((billVal*tipPerc)/100)*100)/100;
+        totalPerPerson = Math.round(((billVal/peopleNum) +(billVal*tipPerc)/100)*100)/100;
+    }
+    if(tipVal>1000)tipVal=convertBig(tipVal);
+    if(totalPerPerson>1000)totalPerPerson=convertBig(totalPerPerson);
+    document.getElementById("tipVal").innerHTML = tipVal;
+    document.getElementById("totalPerPerson").innerHTML = totalPerPerson;
 }
     
